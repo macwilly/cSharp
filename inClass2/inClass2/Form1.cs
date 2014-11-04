e61aaba8f7799d9bf3bca300acd2c5d90101a465
Mackenzie Willard <mtw2935@rit.edu>
2014-11-04 11:27:50 -0500

    added notes

---
+++ inClass2/inClass2/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//also c# does a precompile not on the full thing though 
+namespace inClass2 //this is the name of the project. can help prohibit access. You are enclosing it. Makes it different 
+{
+    public partial class Form1 : Form //partial means that it is not the full class they hide other parts
+    {
+        //this is a constructor
+        public Form1() 
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
