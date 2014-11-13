8fce82615a5fb41f1ff853a01f406700e392f742
macwilly <mtwillard29@gmail.com>
2014-11-13 11:30:11 -0500

    step 1
    Getting text from the text box and checking to see if it is on or off
    Casting as well

--- inClass5/inClass5/Form1.cs
+++ inClass5/inClass5/Form1.cs
@@ -18,7 +18,22 @@ namespace inClass5
         }
         private void allCheckBox_CheckedChanged(object sender, EventArgs e)
         {
+            //how do we know what has been checked or not
+            //sender is just a generic object we need to cast the sender
+            //turn the generic sender to what I want (cast it)
+            CheckBox genCheckBox = sender as CheckBox;
+            //or
+            //CheckBox genCheckBox = (CheckBox) sender CheckBox;
+            //get the text box information
+            string str = genCheckBox.Text;
+            textBoxTextValue.Text = str;
 
+            //find out if it is on or off
+            //String onOff = Convert.ToString(genCheckBox.Checked);
+            //textBoxOnOff.Text = onOff;
+            ///////
+            //textBoxOnOff.Text = Convert.ToString(genCheckBox.Checked);
+            textBoxOnOff.Text = (genCheckBox.Checked).ToString();
         }
     }
 }
