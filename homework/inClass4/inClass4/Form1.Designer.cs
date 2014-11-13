d6890ef82f34ee5b5f0fc4d505be53421ef9c55e
macwilly <mtwillard29@gmail.com>
2014-11-13 12:11:13 -0500

    new for inclass 5

---
+++ homework/inClass4/inClass4/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace inClass4
+{
+    partial class formBeer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBoxXMLRPC = new System.Windows.Forms.GroupBox();
+            this.groupBoxSOAP = new System.Windows.Forms.GroupBox();
+            this.groupBoxRESTFUL = new System.Windows.Forms.GroupBox();
+            this.comboBoxXMLRPC = new System.Windows.Forms.ComboBox();
+            this.comboBoxSOAP = new System.Windows.Forms.ComboBox();
+            this.comboBoxRESTFUL = new System.Windows.Forms.ComboBox();
+            this.buttonXMLRPC = new System.Windows.Forms.Button();
+            this.buttonSOAP = new System.Windows.Forms.Button();
+            this.buttonRESTFUL = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.groupBoxXMLRPC.SuspendLayout();
+            this.groupBoxSOAP.SuspendLayout();
+            this.groupBoxRESTFUL.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(638, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 19);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(48, 19);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // groupBoxXMLRPC
+            // 
+            this.groupBoxXMLRPC.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBoxXMLRPC.Controls.Add(this.buttonXMLRPC);
+            this.groupBoxXMLRPC.Controls.Add(this.comboBoxXMLRPC);
+            this.groupBoxXMLRPC.Location = new System.Drawing.Point(12, 75);
+            this.groupBoxXMLRPC.Name = "groupBoxXMLRPC";
+            this.groupBoxXMLRPC.Size = new System.Drawing.Size(200, 215);
+            this.groupBoxXMLRPC.TabIndex = 1;
+            this.groupBoxXMLRPC.TabStop = false;
+            this.groupBoxXMLRPC.Text = "XML-RPC";
+            // 
+            // groupBoxSOAP
+            // 
+            this.groupBoxSOAP.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBoxSOAP.Controls.Add(this.buttonSOAP);
+            this.groupBoxSOAP.Controls.Add(this.comboBoxSOAP);
+            this.groupBoxSOAP.Location = new System.Drawing.Point(218, 75);
+            this.groupBoxSOAP.Name = "groupBoxSOAP";
+            this.groupBoxSOAP.Size = new System.Drawing.Size(200, 215);
+            this.groupBoxSOAP.TabIndex = 0;
+            this.groupBoxSOAP.TabStop = false;
+            this.groupBoxSOAP.Text = "SOAP";
+            // 
+            // groupBoxRESTFUL
+            // 
+            this.groupBoxRESTFUL.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBoxRESTFUL.Controls.Add(this.buttonRESTFUL);
+            this.groupBoxRESTFUL.Controls.Add(this.comboBoxRESTFUL);
+            this.groupBoxRESTFUL.Location = new System.Drawing.Point(424, 75);
+            this.groupBoxRESTFUL.Name = "groupBoxRESTFUL";
+            this.groupBoxRESTFUL.Size = new System.Drawing.Size(200, 215);
+            this.groupBoxRESTFUL.TabIndex = 2;
+            this.groupBoxRESTFUL.TabStop = false;
+            this.groupBoxRESTFUL.Text = "RESTFUL";
+            // 
+            // comboBoxXMLRPC
+            // 
+            this.comboBoxXMLRPC.FormattingEnabled = true;
+            this.comboBoxXMLRPC.Location = new System.Drawing.Point(6, 50);
+            this.comboBoxXMLRPC.Name = "comboBoxXMLRPC";
+            this.comboBoxXMLRPC.Size = new System.Drawing.Size(121, 26);
+            this.comboBoxXMLRPC.Sorted = true;
+            this.comboBoxXMLRPC.TabIndex = 0;
+            // 
+            // comboBoxSOAP
+            // 
+            this.comboBoxSOAP.FormattingEnabled = true;
+            this.comboBoxSOAP.Location = new System.Drawing.Point(6, 50);
+            this.comboBoxSOAP.Name = "comboBoxSOAP";
+            this.comboBoxSOAP.Size = new System.Drawing.Size(121, 26);
+            this.comboBoxSOAP.Sorted = true;
+            this.comboBoxSOAP.TabIndex = 1;
+            // 
+            // comboBoxRESTFUL
+            // 
+            this.comboBoxRESTFUL.FormattingEnabled = true;
+            this.comboBoxRESTFUL.Location = new System.Drawing.Point(6, 50);
+            this.comboBoxRESTFUL.Name = "comboBoxRESTFUL";
+            this.comboBoxRESTFUL.Size = new System.Drawing.Size(121, 26);
+            this.comboBoxRESTFUL.Sorted = true;
+            this.comboBoxRESTFUL.TabIndex = 1;
+            // 
+            // buttonXMLRPC
+            // 
+            this.buttonXMLRPC.Location = new System.Drawing.Point(52, 113);
+            this.buttonXMLRPC.Name = "buttonXMLRPC";
+            this.buttonXMLRPC.Size = new System.Drawing.Size(75, 23);
+            this.buttonXMLRPC.TabIndex = 3;
+            this.buttonXMLRPC.Text = "Populate";
+            this.buttonXMLRPC.UseVisualStyleBackColor = true;
+            this.buttonXMLRPC.Click += new System.EventHandler(this.buttonXMLRPC_Click);
+            // 
+            // buttonSOAP
+            // 
+            this.buttonSOAP.Location = new System.Drawing.Point(52, 113);
+            this.buttonSOAP.Name = "buttonSOAP";
+            this.buttonSOAP.Size = new System.Drawing.Size(75, 23);
+            this.buttonSOAP.TabIndex = 4;
+            this.buttonSOAP.Text = "Populate";
+            this.buttonSOAP.UseVisualStyleBackColor = true;
+            this.buttonSOAP.Click += new System.EventHandler(this.buttonSOAP_Click);
+            // 
+            // buttonRESTFUL
+            // 
+            this.buttonRESTFUL.Location = new System.Drawing.Point(52, 113);
+            this.buttonRESTFUL.Name = "buttonRESTFUL";
+            this.buttonRESTFUL.Size = new System.Drawing.Size(75, 23);
+            this.buttonRESTFUL.TabIndex = 4;
+            this.buttonRESTFUL.Text = "Populate";
+            this.buttonRESTFUL.UseVisualStyleBackColor = true;
+            this.buttonRESTFUL.Click += new System.EventHandler(this.buttonRESTFUL_Click);
+            // 
+            // formBeer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(638, 441);
+            this.Controls.Add(this.groupBoxRESTFUL);
+            this.Controls.Add(this.groupBoxSOAP);
+            this.Controls.Add(this.groupBoxXMLRPC);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "formBeer";
+            this.Text = "Get  Beers";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBoxXMLRPC.ResumeLayout(false);
+            this.groupBoxSOAP.ResumeLayout(false);
+            this.groupBoxRESTFUL.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBoxXMLRPC;
+        private System.Windows.Forms.GroupBox groupBoxSOAP;
+        private System.Windows.Forms.GroupBox groupBoxRESTFUL;
+        private System.Windows.Forms.Button buttonXMLRPC;
+        private System.Windows.Forms.ComboBox comboBoxXMLRPC;
+        private System.Windows.Forms.Button buttonSOAP;
+        private System.Windows.Forms.ComboBox comboBoxSOAP;
+        private System.Windows.Forms.Button buttonRESTFUL;
+        private System.Windows.Forms.ComboBox comboBoxRESTFUL;
+    }
+}
+
---
+++ homework/inClass4/inClass4/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using CookComputing.XmlRpc;
+
+namespace inClass4
+{
+    [XmlRpcUrl("http://alvin.ist.rit.edu:8100")]
+    public interface IBeers : IXmlRpcProxy
+    {
+        [XmlRpcMethod("beer.getBeers")]
+        string[] getBeers();
+    }
+    public partial class formBeer : Form
+    {
+        public formBeer()
+        {
+            InitializeComponent();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonXMLRPC_Click(object sender, EventArgs e)
+        {
+            IBeers proxy = XmlRpcProxyGen.Create<IBeers>();
+            string[] beers = proxy.getBeers();
+            comboBoxXMLRPC.Items.Clear();
+            for (int i = 0; i<beers.Length; i++)
+            {
+                comboBoxXMLRPC.Items.Add(beers[i]);
+            }
+        }
+
+        private void buttonSOAP_Click(object sender, EventArgs e)
+        {
+            serviceBeer.BeerClient bc = new serviceBeer.BeerClient();
+            string[] beers = bc.getBeers();
+            comboBoxSOAP.Items.Clear();
+            for (int i = 0; i<beers.Length; i++)
+            {
+                comboBoxSOAP.Items.Add(beers[i]);
+            }
+        }
+
+        private void buttonRESTFUL_Click(object sender, EventArgs e)
+        {
+            List<string> beers = new List<string>();
+            string uri = "http://simon.ist.rit.edu:8080/BeerService/resources/Services/Beers";
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(uri);
+            req.Method = "GET";
+
+            try 
+            {
+                HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+
+                Stream str = res.GetResponseStream();
+                XmlReader xr = XmlReader.Create(str);
+                while (xr.Read())
+                {
+                    if (xr.Value != "") comboBoxRESTFUL.Items.Add(xr.Value);
+                }
+                res.Close();
+            
+            }
+            catch(Exception ex)
+            {
+                Console.Write("Error");
+            }
+
+        }
+    }
+}
