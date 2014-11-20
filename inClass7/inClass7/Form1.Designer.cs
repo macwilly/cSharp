74b145ae10347aeaecd37b3190e619046754e0e2
macwilly <mtwillard29@gmail.com>
2014-11-20 11:48:58 -0500

    inClass 7

---
+++ inClass7/inClass7/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace inClass7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGetIt = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonGetIt
+            // 
+            this.buttonGetIt.Location = new System.Drawing.Point(61, 69);
+            this.buttonGetIt.Name = "buttonGetIt";
+            this.buttonGetIt.Size = new System.Drawing.Size(70, 20);
+            this.buttonGetIt.TabIndex = 0;
+            this.buttonGetIt.Text = "Get It";
+            this.buttonGetIt.UseVisualStyleBackColor = true;
+            this.buttonGetIt.Click += new System.EventHandler(this.buttonGetIt_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(61, 120);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(492, 150);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 360);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonGetIt);
+            this.Name = "Form1";
+            this.Text = "In Class 7";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGetIt;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ inClass7/inClass7/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace inClass7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGetIt_Click(object sender, EventArgs e)
+        {
+            // needs to be in Bin/Debug
+            XmlReader xmlFile = XmlReader.Create(@"data.xml");
+            DataSet dataSet = new DataSet();
+
+            //read the xml into dataset
+            dataSet.ReadXml(xmlFile);
+
+            //Pass row table to dataGrid the datasource
+            dataGridView1.DataSource = dataSet.Tables["row"];
+
+            dataGridView1.Columns[0].Visible = false;
+            // OR
+            //dataGridView1.Columns["OrganizationID"].Visible = false;
+
+            
+            xmlFile.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //MessageBox.Show("Clicked");
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //MessageBox.Show("Clicked The Cell");
+            //what one am I clicking on?
+            int col = e.ColumnIndex;//don't really need..
+            int row = e.RowIndex;
+
+            int orgId = Convert.ToInt32(dataGridView1[0, row].Value);
+
+            //getTabs
+            string tabs = "getTabs?orgId=" + orgId;
+            MessageBox.Show(tabs);
+
+
+            //assume with the baseURL that I need {orgId}/General
+            string criteria = orgId + "/General";
+            MessageBox.Show(criteria);
+        }
+    }
+}
