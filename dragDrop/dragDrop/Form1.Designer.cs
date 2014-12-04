e2f02bf7cb25e4971588100a20ec62fc93b5e5fc
macwilly <mtwillard29@gmail.com>
2014-12-04 12:05:39 -0500

    drag and drop

---
+++ dragDrop/dragDrop/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace dragDrop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "One Fish",
+            "Two Fish",
+            "Red Fish",
+            "Blue Fish"});
+            this.listBox1.Location = new System.Drawing.Point(2, 33);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(148, 199);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDown);
+            // 
+            // treeView1
+            // 
+            this.treeView1.AllowDrop = true;
+            this.treeView1.Location = new System.Drawing.Point(308, 33);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(192, 199);
+            this.treeView1.TabIndex = 2;
+            this.treeView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.treeView1_DragDrop);
+            this.treeView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.treeView1_DragEnter);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 352);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ dragDrop/dragDrop/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dragDrop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void treeView1_DragEnter(object sender, DragEventArgs e)
+        {
+            //Console.WriteLine("on");
+            if(e.Data.GetDataPresent("Text"))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void treeView1_DragDrop(object sender, DragEventArgs e)
+        {
+            //Console.WriteLine("Dropped");
+            string item = (string)e.Data.GetData("Text");
+            treeView1.Nodes.Add(item);
+
+        }
+
+        private void listBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //test to see if I've moused downed on an element in the listBox
+            if (this.listBox1.SelectedItem != null)
+            {
+                DoDragDrop(this.listBox1.SelectedItem, DragDropEffects.Copy);
+            }
+
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            Console.WriteLine("Dropped on form");
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            MessageBox.Show("in
+        }
+    }
+}
