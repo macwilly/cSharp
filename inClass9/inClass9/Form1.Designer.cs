107813a8cc9045994782c2773a91b348a1293ff0
macwilly <mtwillard29@gmail.com>
2014-12-04 12:08:14 -0500

    facilities

---
+++ inClass9/inClass9/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace inClass9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonShow = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.TypeID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Type = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Quantity = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Description = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Facilities";
+            // 
+            // buttonShow
+            // 
+            this.buttonShow.Location = new System.Drawing.Point(12, 101);
+            this.buttonShow.Name = "buttonShow";
+            this.buttonShow.Size = new System.Drawing.Size(126, 23);
+            this.buttonShow.TabIndex = 1;
+            this.buttonShow.Text = "Populate View";
+            this.buttonShow.UseVisualStyleBackColor = true;
+            this.buttonShow.Click += new System.EventHandler(this.buttonShow_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.TypeID,
+            this.Type,
+            this.Quantity,
+            this.Description});
+            this.listView1.Location = new System.Drawing.Point(225, 27);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(340, 195);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // TypeID
+            // 
+            this.TypeID.Text = "Type ID";
+            this.TypeID.Width = 0;
+            // 
+            // Type
+            // 
+            this.Type.Text = "Type";
+            // 
+            // Quantity
+            // 
+            this.Quantity.Text = "Quantity";
+            // 
+            // Description
+            // 
+            this.Description.Text = "Description";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 261);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.buttonShow);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonShow;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader TypeID;
+        private System.Windows.Forms.ColumnHeader Type;
+        private System.Windows.Forms.ColumnHeader Quantity;
+        private System.Windows.Forms.ColumnHeader Description;
+    }
+}
+
---
+++ inClass9/inClass9/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;//allow us to have a stop watch
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace inClass9
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch sw = new Stopwatch();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonShow_Click(object sender, EventArgs e)
+        {
+            // play with stopwatch
+            sw.Reset();
+            sw.Start();
+            //load xml documents and GetElementsByTagName()
+            string uri = @"http://simon.ist.rit.edu:8080/Services/resources/ESD/1/Facilities";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            HttpWebResponse resp = (HttpWebResponse)request.GetResponse();
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(resp.GetResponseStream());
+
+            XmlNodeList elementList = xmlDoc.GetElementsByTagName("facility");
+            for (int i = 0; i < elementList.Count;i++ )
+            {
+                string[] arr = new string[4];
+                arr[0] = elementList[i].SelectSingleNode("typeId").InnerText;
+                arr[1] = elementList[i].SelectSingleNode("type").InnerText;
+                arr[2] = elementList[i].SelectSingleNode("quantity").InnerText;
+                arr[3] = elementList[i].SelectSingleNode("description").InnerText;
+
+                ListViewItem itm;
+                itm = new ListViewItem(arr);
+                listView1.Items.Add(itm);
+                listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            }
+
+            sw.Stop();
+            MessageBox.Show(sw.ElapsedMilliseconds.ToString() + "ms");
+        }
+    }
+}
